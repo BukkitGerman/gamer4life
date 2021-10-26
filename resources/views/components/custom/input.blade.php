aa099b92891a5090d75c55819952e0fc91a7e227
Justin Preuß <justin.preuss@aks-service.de>
2021-10-26 09:29:35 +0200

    add $inputClasses to the input component

--- resources/views/components/custom/input.blade.php
+++ resources/views/components/custom/input.blade.php
@@ -2,7 +2,7 @@
     @if(isset($labelkey))
         <label for="{{ $id }}">{{ __($labelkey) }}</label>
     @endif
-    <input class="form-control" id="{{ $id }}" name="{{ $name }}" type="{{ $type }}">
+    <input class="form-control {{ $inputClasses ?? ''}}" id="{{ $id }}" name="{{ $name }}" type="{{ $type }}">
 </div>
 @if ($errors->has($name))
     <p class="text-danger p-0 m-0">{{ $errors->first($name) }}</p>
