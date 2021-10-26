fdfdd1b1f54670721a9abaf405959488b6c59184
Justin Preuß <justin.preuss@aks-service.de>
2021-10-26 10:16:03 +0200

    fix button id. The id is now optional

--- resources/views/components/custom/button.blade.php
+++ resources/views/components/custom/button.blade.php
@@ -1,4 +1,4 @@
-<button class="btn {{ $classes ?? '' }}" id="{{ $id }}" name="{{ $name ?? ''}}" type="{{ $type }}">
+<button class="btn {{ $classes ?? '' }}" id="{{ $id ?? ''}}" name="{{ $name ?? ''}}" type="{{ $type }}">
     @if(isset($icon))
         <i class="{{$icon}}"></i>{{ __($translationKey ?? '') }}
     @else
