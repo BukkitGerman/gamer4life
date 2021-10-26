55e6a3d3ae3daad38be4c955006db89d787e749f
Justin Preuß <justin.preuss@aks-service.de>
2021-10-26 08:43:31 +0200

    add form-control class to the input field in the username component

--- resources/views/components/custom/username.blade.php
+++ resources/views/components/custom/username.blade.php
@@ -2,7 +2,7 @@
     @if(isset($labelkey))
         <label for="username">{{ __($labelkey) }}</label>
     @endif
-    <input type="text" id="username" name="username" placeholder="{{ $placeholder ?? '' }}"/>
+    <input class="form-control" type="text" id="username" name="username" placeholder="{{ $placeholder ?? '' }}"/>
 </div>
 @if ($errors->has('username'))
     <p class="text-danger p-0 m-0">{{ $errors->first('username') }}</p>
